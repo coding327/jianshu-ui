1894de0af3dd13c3751e75caf3cdd02d9dcf26e1
black jack <1545721718@qq.com>
2024-03-25 21:44:32 +0800

    feat: 文件夹命名改为小写

---
+++ src/views/fans/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>粉丝管理</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  },
+}
+
+</script>
+<style scoped>
+</style>
