bd57f32a3cdf7a3f3671afffd4b47b707d327592
1545721718@qq.com <1545721718@qq.com>
2024-03-31 20:15:19 +0800

    🔧 build: 添加生产环境配置文件

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.VUE_APP_PUBLIC_PATH,
   devServer: {
     proxy: {
       '/api': {
