f69928bb3221b800c3c08b404645f80edf572bbb
1545721718@qq.com <1545721718@qq.com>
2024-04-05 18:34:11 +0800

    ✨ feat: 添加缓存常量

---
+++ src/constants/cache.js
@@ -0,0 +1,7 @@
+// token key
+export const TOKEN_KEY = 'TOKEN__'
+
+// user info key
+export const USERINFO_KEY = 'USERINFO__'
+export const USERNAME_KEY = 'USERNAME__'
+export const PASSWORD_KEY = 'PASSWORD__'
