10f66c4ae8c209702b49adcf7a31f22fa08178d4
1545721718@qq.com <1545721718@qq.com>
2024-04-05 21:08:24 +0800

    🦄 refactor: 添加登录页面

---
+++ src/views/login/index.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="login">
+    <el-card>
+      <div class="login-title">{{ PROJECT_NAME }}</div>
+      <el-form ref="loginFormRef" :model="loginForm" :rules="loginRules">
+        <el-form-item prop="username">
+          <el-input
+            ref="username"
+            v-model="loginForm.username"
+            placeholder="请输入用户名"
+          >
+            <template #prefix>
+              <i class="el-icon-user"></i>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            ref="password"
+            v-model="loginForm.password"
+            type="password"
+            placeholder="请输入密码"
+            show-password
+            autocomplete="new-password"
+          >
+            <template #prefix>
+              <i class="el-icon-lock"></i>
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item>
+          <div class="login-btn">
+            <el-button
+              :loading="loading"
+              @click.native.prevent="handleLogin($refs.loginFormRef)"
+              type="primary"
+              >登录</el-button
+            >
+          </div>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+import { PROJECT_NAME } from "@/project-config";
+
+export default {
+  data() {
+    return {
+      loginForm: {
+        username: "",
+        password: "",
+      },
+      loginRules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+        ],
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }],
+      },
+      loading: false,
+    };
+  },
+  components: {},
+  mounted() {
+    if (this.loginForm.username === "") {
+      this.$refs.username.focus();
+    } else if (this.loginForm.password === "") {
+      this.$refs.password.focus();
+    }
+  },
+  methods: {
+    ...mapActions("loginModule", ["accountLoginAction"]),
+    handleLogin(formEl) {
+      if (!formEl) return;
+      formEl.validate(async (valid) => {
+        if (!valid) return;
+        this.loading = true;
+        try {
+          await this.accountLoginAction(this.loginForm);
+        } finally {
+          this.loading = false;
+        }
+      });
+    },
+  },
+};
+</script>
+<style scoped>
+/* 登录页面背景图片样式 */
+.login {
+  width: 100%;
+  height: 100%;
+  background: url("../../assets/login-bg.png") no-repeat center center / 100%
+    100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+/* el-card卡片组件样式 */
+.el-card {
+  width: 420px;
+  height: 300px;
+}
+/* 登录标题样式 */
+.login-title {
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 50px;
+  color: #333;
+}
+/* 登录按钮 */
+.login-btn {
+  text-align: right;
+}
+.el-switch {
+  margin: 0 20px 0 8px;
+}
+</style>
